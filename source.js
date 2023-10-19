1f301343580fc545eeeef84e81ee532ffd0c86ca
김널븐 <147522293+neolbeun@users.noreply.github.com>
2023-10-19 23:14:59 +0900

    Update source.js

--- source.js
+++ source.js
@@ -1,4 +1,30 @@
-function showElementByName(id) {
+function setDisplayElementByName(id,display) {
+	document.getElementById(id).style.display = display;
+	
+	return;
+}
+
+function _resetForm(id) {
 	const el = document.getElementById(id);
-	el.style.display = "block";
+
+	// set select as default option (맑음)
+	el.lastElementChild.selectedIndex = 0;
+	
+	return;
+}
+
+function resetFroms() {
+	const selectList = ["12pm", "1pm"];
+	const firstSelect = "10am";
+	
+	for (sel of selectList) {
+		_resetForm(sel);
+		setDisplayElementByName(sel, "none");
+	}
+	
+	_resetForm(firstSelect);
+	
+	setDisplayElementByName("resetBtn", "none");
+	
+	return;
 }
